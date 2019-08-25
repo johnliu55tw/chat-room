a47cd74ddd2605322d0ab9636a68bf5fae31016c
John Liu <johnliu55tw@gmail.com>
2019-08-25 14:38:22 +0800

    Use LimitOffsetPagination for pagination

--- server/chat_room_server/chat_room_server/settings.py
+++ server/chat_room_server/chat_room_server/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
