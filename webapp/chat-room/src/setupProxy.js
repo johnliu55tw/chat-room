724d188b55e80112e00a48e663ec4efad8e6ba3b
John Liu <johnliu55tw@gmail.com>
2019-09-01 22:18:28 +0800

    Setup frontend dev proxy to /api

--- webapp/chat-room/src/setupProxy.js
+++ webapp/chat-room/src/setupProxy.js
@@ -2,4 +2,5 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(proxy('/ws', {target: 'ws://localhost:8000', ws: true}));
+  app.use(proxy('/api', {target: 'http://localhost:8000'}));
 };
